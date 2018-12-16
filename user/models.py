98886aff3b3f67a0a4e4fd7e2c11ae0ddb3cc928
willow <hardtosaygoodbye@sina.com>
2018-12-16 15:37:20 +0800

    feat(user):新增用户名

--- user/models.py
+++ user/models.py
@@ -3,6 +3,7 @@ from uuid import uuid4
 
 class User(Model):
     phone = CharField('手机号', max_length = 11)
+    username = CharField('用户名', max_length = 15, default = '悦租房用户')
     password = CharField('密码', max_length = 15, default = '1')
     def __str__(self):
         return self.phone
