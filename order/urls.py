db7aa36eeae6ddd95bf2d14ed7e3373b40f14b89
willow <hardtosaygoodbye@sina.com>
2018-12-22 13:53:13 +0800

    feat(order):add cancel order url

--- order/urls.py
+++ order/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^order/$', OrderView.as_view()),
     url(r'^discuss_order/$', DiscussOrderView.as_view()),
     url(r'^pay_order/$', PayOrderView.as_view()),
+    url(r'^cancel_order/$', CancelOrderView.as_view()),
 ]
